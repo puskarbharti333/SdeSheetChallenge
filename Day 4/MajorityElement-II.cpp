c6456f79a2fcc674e7cfeac46e5fbf4de60cde24
puskarbharti333 <puskarbharti333@gmail.com>
2022-06-09 16:49:49 +0530

    Majority Element II

---
+++ Day 4/MajorityElement-II.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h> 
+vector<int> majorityElementII(vector<int> &arr)
+{
+    vector<int> ans;
+    int count1 = 0, count2 = 0, num1 = -1, num2 = -1;
+    int temp = 0;
+    
+    for(int x:arr){
+        if(x == num1) count1++;
+        else if (x == num2) count2++;
+        else if(count1 == 0){
+            num1 = x;
+            count1 = 1;
+        }
+        else if(count2 == 0){
+            num2 = x; 
+            count2 = 1;
+        }
+        else{
+            count1--; count2--;
+        }
+    }
+    
+    for(int x : arr){
+        if(x==num1) temp++;
+    }
+    if(temp > (arr.size()/3)) ans.push_back(num1);
+    
+    temp = 0;
+    for(int x : arr){
+        if(x==num2) temp++;
+    }
+    if(temp > (arr.size()/3)) ans.push_back(num2);
+    
+    return ans;
+
+}
